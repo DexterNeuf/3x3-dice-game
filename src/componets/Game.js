e36ed2ce15b48dd5de543b1862973d4ff42d3c65
Dex <dxneufeld14@gmail.com>
2023-07-22 16:43:37 -0400

    deleted absolete code

--- src/componets/Game.js
+++ src/componets/Game.js
@@ -11,44 +11,7 @@ function Game() {
     0, 0, 0, 0, 0, 0, 0, 0, 0,
   ]);
   const [lastAdded, newAdd] = useState([0, 0]);
-  const [test, setTest] = useState([0, 0, '']);
-
-  useEffect(() => {
-    if (lastAdded[0] === 1) {
-      checkDuplicateValues(0);
-    } else if (lastAdded[0] === 2) {
-      checkDuplicateValues(3);
-    } else if (lastAdded[0] === 3) {
-      checkDuplicateValues(6);
-    }
-  }, [isPlayerTurn]);
-
-  const checkDuplicateValues = (rowNum) => {
-    //player went last
-    if (!isPlayerTurn) {
-      for (let i = 0; i < 3; i++) {
-        //checks to see if dice value just played matches up with any dice of the same
-        //value in the oppents row
-        if (
-          opponentBoardValues[i + rowNum] === playerBoardValues[lastAdded[1]]
-        ) {
-          setTest([lastAdded[0], playerBoardValues[lastAdded[1]], 'opp']);
-        }
-      }
-    }
-    //opp went last
-    else {
-      for (let i = 0; i < 3; i++) {
-        //checks to see if dice value just played matches up with any dice of the same
-        //value in the player row
-        if (
-          playerBoardValues[i + rowNum] === opponentBoardValues[lastAdded[1]]
-        ) {
-          setTest([lastAdded[0], opponentBoardValues[lastAdded[1]], 'player']);
-        }
-      }
-    }
-  };
+ 
 
     
   return (
@@ -83,7 +46,6 @@ function Game() {
             playerTurn={isPlayerTurn}
             diceRolled={isDiceRolled}
             isPlayer={true}
-            test={test}
           />
           <DiceBoard
             diceNumber={diceNumber}
@@ -94,7 +56,6 @@ function Game() {
             playerTurn={isPlayerTurn}
             diceRolled={isDiceRolled}
             isPlayer={false}
-            test={test}
           />
         </div>
       </fieldset>
